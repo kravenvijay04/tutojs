426bdeb33b03074585559470b82cdec3dd8f9581
kravenvijay04 <123456789+odin@users.noreply.github.com>
2024-02-20 01:06:32 +0530

    Delete function is added

--- Studentform/script.js
+++ Studentform/script.js
@@ -1,14 +1,21 @@
 var name1 = document.getElementById("Name")
 var age1 = document.getElementById("Age")
-var gender1 = document.getElementById("Name")
-var course1 = document.getElementById("Name")
-var email = document.getElementById("Name")
+var gender1 = document.getElementById("gender")
+var course1 = document.getElementById("course")
+var email = document.getElementById("mail")
 var result = document.getElementById("res")
 //var res1=result.textContent
 function call(){
     var tr = document.createElement("tr")
-    tr.innerHTML= "<td id='res1' onclick()='call()'></td>"
-    tr.textContent="hello"
+    tr.innerHTML=`<td>${name1.value}</td>
+    <td>${age1.value}</td>
+    <td>${gender1.value}</td>
+    <td>${course1.value}</td>
+    <td>${email.value}</td>
+    <td><button onclick="delete1(event)">delete</button></td>`
     result.append(tr)
-    console.log(tr)
+}
+function delete1(event){
+    var close=event.target.closest("tr")
+    close.remove()
 }
