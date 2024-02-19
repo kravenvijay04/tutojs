ca1da79a12ba7f30dc6c3e4e141bead6c9639460
kravenvijay04 <123456789+odin@users.noreply.github.com>
2024-02-20 01:43:56 +0530

    small modifications

--- Studentform/script.js
+++ Studentform/script.js
@@ -4,7 +4,7 @@ var gender1 = document.getElementById("gender")
 var course1 = document.getElementById("course")
 var email = document.getElementById("mail")
 var result = document.getElementById("res")
-//var res1=result.textContent
+
 function call(){
     var tr = document.createElement("tr")
     tr.innerHTML=`<td>${name1.value}</td>
@@ -15,6 +15,7 @@ function call(){
     <td><button onclick="delete1(event)" id="button1" >delete</button></td>` 
     result.append(tr)
 }
+
 function delete1(event){
     var close=event.target.closest("tr")
     close.remove()
