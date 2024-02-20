b26c65f7e8235dd4a6e02ef35d0741e9096d40e0
kravenvijay04 <123456789+odin@users.noreply.github.com>
2024-02-20 13:41:30 +0530

    popup feature is added

---
+++ Booksky/function.js
@@ -0,0 +1,8 @@
+var bookoverlay=document.querySelector(".overlay")
+var bookpopup=document.querySelector(".pop-book")
+var addbut1=document.querySelector(".but")
+
+addbut1.addEventListener("click",function(){
+    bookoverlay.style.display="block"
+    bookpopup.style.display="block"
+})
